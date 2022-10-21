64a5318f7c5968a3afdec9c5dbce37e7260ae3fc
JayNisbett <jay@setshowsold.com>
2022-10-21 17:59:33 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,70 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import DataTable from "react-data-table-component";
+import "./styles.css";
+import downline from "./dummy/downline";
 
-// Render each post
-function renderPost(post){
-  const { data: { title, url, author, id } } = post
-  const authorUrl = `https://www.reddit.com/u/${author}`
+const colors = require("tailwindcss/colors");
 
+const columns = [
+  {
+    id: 1,
+    name: "Upline Agent Code",
+    selector: (row) => row.UplineAgentCode,
+    sortable: true,
+    reorder: true
+  },
+  {
+    id: 2,
+    name: "Downline Agent Code",
+    selector: (row) => row.DownlineAgentCode,
+    sortable: true,
+    reorder: true
+  },
+  {
+    id: 3,
+    name: "Downline Level",
+    selector: (row) => row.DownlineLevel,
+    sortable: true,
+    right: true,
+    reorder: true
+  },
+  {
+    id: 4,
+    name: "IN Base Shop Ind",
+    selector: (row) => row.InBaseShopInd,
+    sortable: true,
+    right: true,
+    reorder: true
+  },
+  {
+    id: 5,
+    name: "Upline Agent Email",
+    selector: (row) => row.UplineAgentEmail,
+    sortable: true,
+    right: true,
+    reorder: true,
+    cell: (row) => (
+      <a href="mailto:{row.UplineAgentEmail}">{row.UplineAgentEmail}</a>
+    )
+  },
+  {
+    id: 6,
+    name: "Downline Agent Email",
+    selector: (row) => row.DownlineAgentEmail,
+    sortable: true,
+    right: true,
+    reorder: true
+  }
+];
+
+const conditionalRowStyles = [
+  {
+    when: (row) => row.DownlineLevel >= 2,
+    style: {
+      backgroundColor: "emerald-900",
+      color: "white",
+      "&:hover": {
+        cursor: "pointer"
+      }
+    }
+  },
+  // You can also pass a callback to style for additional customization
+  {
+    when: (row) => row.calories < 400,
+    style: (row) => ({ backgroundColor: row.isSpecial ? "pink" : "inerit" })
+  }
+];
+
+function Downline() {
   return (
-    <div className="reddit_widget__post" key={id}>
-      <div className="reddit_widget__posted_by">
-        posted by <a href={authorUrl} className="reddit_widget__posted_by" target="_blank" rel="noopener noreferrer">u/{author}</a>
-      </div>
-      <a href={url} className="reddit_widget__title" target="_blank" rel="noopener noreferrer">{title}</a>
-    </div>
-  )
-}
-
-// Filter, since reddit always returns stickied posts up top
-function nonStickiedOnly(post){
-  return !post.data.stickied
-}
-
-function App({ domElement }) {
-  const subreddit = domElement.getAttribute("data-subreddit")
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState('');
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from reddit
-    setLoading(true)
-    fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        setLoading(false);
-        setData(data.data.children.slice(0, 10));
-      })
-      .catch((e) => {
-        console.log(e)
-        setLoading(false);
-        setError('error fetching from reddit');
-      });
-  }, [ subreddit ])
-
-  return (
-    <div className="reddit_widget__app">
-      <h1 className="reddit_widget__header">
-        Latest posts in <a href={`https://reddit.com/r/${subreddit}`} rel="noopener noreferrer">/r/{subreddit}</a>
-      </h1>
-      <div className="reddit_widget__inner">
-        {loading && "Loading..."}
-        {error && error}
-        {!!data.length && data.filter(nonStickiedOnly).map(renderPost)}
-      </div>
-      <p className="reddit_widget__powered_by">
-        This widget is powered by{" "}
-        <a
-          href="https://javascriptpros.com"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          JavaScriptPros.com
-        </a>
-      </p>
+    <div className="App">
+      <DataTable
+        title="Downline"
+        columns={columns}
+        data={downline}
+        defaultSortFieldId={1}
+        pagination
+        selectableRows
+        conditionalRowStyles={conditionalRowStyles}
+      />
     </div>
   );
 }
-
-export default App;
+export default Downline;
---
+++ src/dummy/downline.js
@@ -0,0 +1,122 @@
+export default [
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0028552",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "Aubreyc151@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0028256",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sfg.bbrenes@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0023176",
+        DownlineLevel: "2",
+        InBaseShopInd: "0",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "c.david.hernandez.1969@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0031779",
+        DownlineLevel: "4",
+        InBaseShopInd: "0",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "dajehp@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0017687",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "dmuh27@yahoo.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0027106",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "daviddaka5@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0026734",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sfg.dhen@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0023615",
+        DownlineLevel: "2",
+        InBaseShopInd: "0",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "gzompa@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0028064",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sfg.ialvarez@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0031686",
+        DownlineLevel: "2",
+        InBaseShopInd: "0",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sfg.jvelasquezagency@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0026269",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sfg.jmartinez@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0030773",
+        DownlineLevel: "2",
+        InBaseShopInd: "0",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "rtgallon@aol.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0027470",
+        DownlineLevel: "1",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "robertrivas687@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0031548",
+        DownlineLevel: "2",
+        InBaseShopInd: "1",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sfg.schavez@gmail.com"
+    },
+    {
+        UplineAgentCode: "SFG0023055",
+        DownlineAgentCode: "SFG0031587",
+        DownlineLevel: "2",
+        InBaseShopInd: "0",
+        UplineAgentEmail: "sfg.fbrenesagency@gmail.com",
+        DownlineAgentEmail: "sthein75@yahoo.com"
+    }
+];
