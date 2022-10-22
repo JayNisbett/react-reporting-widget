27bb6c674f17f8eb539b6287307e374b2dadfcfc
JayNisbett <19826552+JayNisbett@users.noreply.github.com>
2022-10-22 16:35:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Downline from './App';
 
 // let widget = `<div style="margin: 100px auto; max-width: 500px;"><div class="reddit_widget" data-subreddit="javascript"></div></div>`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Downline />
   </React.StrictMode>,
   document.getElementById('dashboard')
 );
