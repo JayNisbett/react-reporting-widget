31a66921a64461bb0d76e89ce67ea44f4c8774e8
JayNisbett <19826552+JayNisbett@users.noreply.github.com>
2022-10-22 16:43:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import Downline from './App';
 
 // let widget = `<div style="margin: 100px auto; max-width: 500px;"><div class="reddit_widget" data-subreddit="javascript"></div></div>`;
 
+let target = document.querySelector('hl_dashboard--website-visitors');
 ReactDOM.render(
-  <React.StrictMode>
-    <Downline />
-  </React.StrictMode>,
-  document.querySelector('hl_dashboard--website-visitors')
+    <Downline />, target
 );
